dc9ea93f28a067f033277341b497e350debe9ef5
Tim Oltman <toltman@gmail.com>
2020-04-14 07:30:48 -0400

    added comments

--- quote.js
+++ quote.js
@@ -1,10 +1,15 @@
 const api = require("./api.json");
 const https = require("https");
 
+// Print the stock price
 function printQuote(stock, price) {
   console.log(`The price for ${stock} is ${price}`);
 }
 
+/**
+ * Gets the current price for a stock
+ * @param {String} query Ticker symbol for the the stock.
+ */
 function get(query) {
   const request = https.get(
     `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${query}&apikey=${api.key}`,
